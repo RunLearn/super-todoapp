28da4b985187ff96305dbf03b9b18f1dc45b383e
jisu <ji_su1107@likelion.org>
2022-02-17 22:18:49 +0900

    style: Apply global style at _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import { store } from '../src/store'
+import GlobalStyle from '../styles/global-styles'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={ store } >
+      <GlobalStyle />
       <Component {...pageProps} />
     </Provider>
   )
