0dcd24950bd4ccf6d264056fe5617e91974d57f6
merge-simpson <letsdev@kakao.com>
2022-02-21 17:29:39 +0900

    style: Add theme for buttons

---
+++ src/features/common/domain/ButtonType.ts
@@ -0,0 +1,6 @@
+type ButtonType = 
+"main" | "sub" | "primary" | "secondary" |
+"success" | "warning" | "danger" | "info" |
+"light" | "dark"
+
+export default ButtonType
--- styles/theme.ts
+++ styles/theme.ts
@@ -1,12 +1,66 @@
 import { DefaultTheme } from "styled-components";
+import ButtonType from "../src/features/common/domain/ButtonType";
+
+const palette = {
+  main: "#7b9acc",
+  sub: "#FCF6F5",
+  lightGray: '#AAAAAA',
+  gray: "gray",
+  grey: "grey",
+  darkGray: '#444444',
+}
+
+const buttonPalette = {
+  main: "#7b9acc",
+  sub: "#FCF6F5",
+  primary: "blue",
+  secondary: palette.lightGray,
+  success: "green",
+  warning: "yellow",
+  danger: "red",
+  info: "blueish",
+  light: "white",
+  dark: "black",
+}
+
+const fontColor = {
+  main: "#FCF6F5",
+  sub: "#7b9acc",
+  primary: "white",
+  secondary: "black",
+  success: "white",
+  warning: "black",
+  danger: "white",
+  info: "white",
+  light: "black",
+  dark: "white",
+}
+
+const styleSet = {
+  buttons : Object.keys(buttonPalette).map((keyName, index, array) => {
+    const className = <ButtonType>keyName
+    return `
+      &.${className} {
+        background-color: ${ buttonPalette[className] };
+        color: ${ fontColor[className] };
+        ${
+          className === 'light' && `
+            border-style: solid;
+            border-color: rgb(200,200,200, 0.5);
+          `
+        }
+      }
+    `
+  }),
+}
 
 const theme: DefaultTheme = {
   basicWidth: "320px",
-
-  color: {
-    main: "#1c1f25",
-    sub: "#fff"
-  }
+  ...palette,
+  buttonPalette,
+  fontColor,
+  styleSet,
+  shadowMedium: "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
 };
 
 const nextTheme: DefaultTheme = {
