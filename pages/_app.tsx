32850ab656ed46ee387e785a65c0a63d4a3b0a90
merge-simpson <letsdev@kakao.com>
2022-02-21 17:55:04 +0900

    Style: Apply <GlobalStyle />

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import { ThemeProvider } from 'styled-components'
 import { store } from '../src/store'
+import GlobalStyle from '../styles/global-styles'
 import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={ store } >
       <ThemeProvider theme={ theme }>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </Provider>
