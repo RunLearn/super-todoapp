4fa5edad33347c1c8cecce49ef683b3328f9dec3
merge-simpson <letsdev@kakao.com>
2022-02-22 00:36:13 +0900

    Feat: Add Nav to Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
+import Nav from '@common/components/Nav'
 import type { NextPage } from 'next'
+import MainLayout from 'styles/divs/MainLayout'
 
 const Home: NextPage = () => {
   return (
     <>
-      <p>Just Page</p>
+      <header>
+        <Nav selectedIndex={0} />
+      </header>
+      <MainLayout>
+        <h3>Just Page</h3>
+      </MainLayout>
     </>
   )
 }
