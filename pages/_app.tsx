65f7715c3bae80a0e2ae146359b9178caff9a093
merge-simpson <letsdev@kakao.com>
2022-02-21 17:39:15 +0900

    Build(_app.tsx): Apply ThemeProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
+import { ThemeProvider } from 'styled-components'
 import { store } from '../src/store'
+import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={ store } >
-      <Component {...pageProps} />
+      <ThemeProvider theme={ theme }>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </Provider>
   )
 }
