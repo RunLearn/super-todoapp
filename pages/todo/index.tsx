f5cc28f3ff6f0b132d1c15471d1ad67ee7ec3ffc
merge-simpson <letsdev@kakao.com>
2022-02-27 01:17:02 +0900

    Feat(Todo): Set Layout

--- pages/todo/index.tsx
+++ pages/todo/index.tsx
@@ -1,5 +1,7 @@
 import Nav from "@common/components/Nav"
+import TodoListConatiner from "@todo/components/TodoListConatiner"
 import { NextPage } from "next"
+import FlexBoxLayout from "styles/divs/FlexBoxLayout"
 import MainLayout from "styles/divs/MainLayout"
 
 const TodoPage: NextPage = () => {
@@ -9,7 +11,11 @@ const TodoPage: NextPage = () => {
                 <Nav menuIndex={1} />
             </header>
             <MainLayout>
-                <h3>Todo List</h3>
+                <FlexBoxLayout flexType="rowAround">
+                    <TodoListConatiner title="Todo" list={ undefined } />
+                    <TodoListConatiner title="Doing" list={ undefined } backgroundColor="#cc9a7b" />
+                    <TodoListConatiner title="Done" list={ undefined } backgroundColor="#9acc7b" />
+                </FlexBoxLayout>
             </MainLayout>
         </>
     )
