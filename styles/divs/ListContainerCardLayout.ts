0cfb7bf45172b5ab5e0b81ab3a2c31fc8425ebaa
merge-simpson <letsdev@kakao.com>
2022-02-27 00:07:21 +0900

    Style(ListConatinerCardLayout): Use CSS var

--- styles/divs/ListContainerCardLayout.ts
+++ styles/divs/ListContainerCardLayout.ts
@@ -2,21 +2,31 @@ import styled, { css } from "styled-components";
 
 interface ListContainerCardLayoutProps {
     backgroundColor?: string;
-    fontColor?: string;
+    titleColor?: string;
 }
 
 export const ListContainerCardLayout = styled.div<ListContainerCardLayoutProps>`
     ${(props) =>  props.theme.styleSet.flex.colCenter}
 
-    ${(props) => css`
-        --sub: ${props.theme.sub};
-    `}
+    ${(props) => {
+        const { func } = props.theme
 
-    background-color: ${(props) => props.theme.func.hexWithOpacity(props.backgroundColor ?? props.theme.main, 0.5)};
+        return css`
+            --bg-color: ${func.hexWithOpacity(props.backgroundColor ?? props.theme.main, 0.5)};
+            --title-color: ${ props.backgroundColor ?? props.theme.sub };
+            --shadow: ${props.theme.shadowLarge};
+    `}}
+
+    background-color: var(--bg-color);
     border-radius: 5px;
     box-shadow: ${props => props.theme.shadowLarge};
-    color: ${(props) => props.backgroundColor ?? props.theme.sub};
-    font-weight: bold;
-
     padding: 1rem;
+
+    header {
+        color: var(--title-color);
+        font-weight: bold;
+        text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.1);
+        padding-bottom: 0.5rem;
+        width: 100%;
+    }
 `;
