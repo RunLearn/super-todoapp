da98008aec6ca8cf5cb0e6f5fe4045a86a45eb4e
merge-simpson <letsdev@kakao.com>
2022-03-03 00:41:59 +0900

    Style(Theme - Flex): Use keyof typeof { ... }

--- styles/styleSet/flex.ts
+++ styles/styleSet/flex.ts
@@ -1,110 +1,103 @@
-export type FlexType =
-    'colCenter' | 'colLeft' |
-    'colRight' | 'colRightFliplr' | 'colRightMiddleFliplr' |
-    'rowAround' | 'rowAroundMiddle' | 'rowAroundBottom' |
-    'rowBetween' | 'rowBetweenMiddle' | 'rowBetweenBottom' |
-    'rowLeft' | 'rowLeftMiddle' |
-    'rowRight' | 'rowRightMiddle' |
-    'rowCenter' | 'rowCenterMiddle'
-
 const flex = {
-    colCenter: `
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-    `,
-    colLeft: `
-      display: flex;
-      flex-direction: column;
-      align-items: start;
-    `,
-    colRight: `
-      display: flex;
-      flex-direction: column;
-      align-items: flex-end;
-    `,
-    colRightFliplr: `
+  colCenter: `
     display: flex;
-    flex-direction: row-reverse;
-    align-items: start;
-    `,
-    colRightMiddleFliplr: `
-    display: flex;
-    flex-direction: row-reverse;
+    flex-direction: column;
     align-items: center;
-    `,
-    rowAround: `
-      display: flex;
-      flex-direction: row;
-      justify-content: space-around;
-      align-items: start;
-    `,
-    rowAroundMiddle: `
-      display: flex;
-      flex-direction: row;
-      justify-content: space-around;
-      align-items: center;
-    `,
-    rowAroundBottom: `
-      display: flex;
-      flex-direction: row;
-      justify-content: space-around;
-      align-items: flex-end;
-    `,
-    rowBetween: `
-      display: flex;
-      flex-direction: row;
-      justify-content: space-between;
-      align-items: start;
-    `,
-    rowBetweenMiddle: `
-      display: flex;
-      flex-direction: row;
-      justify-content: space-between;
-      align-items: center;
-    `,
-    rowBetweenBottom: `
-      display: flex;
-      flex-direction: row;
-      justify-content: space-between;
-      align-items: flex-end;
-    `,
-    rowLeft: `
-      display: flex;
-      flex-direction: row;
-      justify-content: start;
-      align-items: start;
-    `,
-    rowLeftMiddle: `
-      display: flex;
-      flex-direction: row;
-      justify-content: start;
-      align-items: center;
-    `,
-    rowRight: `
-      display: flex;
-      flex-direction: row;
-      justify-content: flex-end;
-      align-items: start;
-    `,
-    rowRightMiddle: `
-      display: flex;
-      flex-direction: row;
-      justify-content: flex-end;
-      align-items: center;
-    `,
-    rowCenter: `
-      display: flex;
-      flex-direction: row;
-      justify-content: center;
-      align-items: start;
-    `,
-    rowCenterMiddle: `
-      display: flex;
-      flex-direction: row;
-      justify-content: center;
-      align-items: center;
-    `,
-  }
+  `,
+  colLeft: `
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+  `,
+  colRight: `
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+  `,
+  colRightFliplr: `
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: start;
+  `,
+  colRightMiddleFliplr: `
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+  `,
+  rowAround: `
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: start;
+  `,
+  rowAroundMiddle: `
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+  `,
+  rowAroundBottom: `
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: flex-end;
+  `,
+  rowBetween: `
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: start;
+  `,
+  rowBetweenMiddle: `
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  `,
+  rowBetweenBottom: `
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: flex-end;
+  `,
+  rowLeft: `
+    display: flex;
+    flex-direction: row;
+    justify-content: start;
+    align-items: start;
+  `,
+  rowLeftMiddle: `
+    display: flex;
+    flex-direction: row;
+    justify-content: start;
+    align-items: center;
+  `,
+  rowRight: `
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: start;
+  `,
+  rowRightMiddle: `
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: center;
+  `,
+  rowCenter: `
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: start;
+  `,
+  rowCenterMiddle: `
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  `,
+}
 
-  export default flex
+export type FlexType = keyof typeof flex
+
+export default flex
